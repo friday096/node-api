fad09a8bd67c30ca6fd02c750ce41b74f86d5078
friday096 <ssantoshh1996@gmail.com>
2023-11-24 18:36:48 +0530

    middleware-auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,6 +17,24 @@ verifyToken = (req, res, next) => {
     req.secret = process.env.JWT_SECRET_KEY;
     next();
   })
+
+// 2nd Method
+// if (req.headers['authorization']) {
+//   try {
+//       let authorization = req.headers['authorization'].split(' ');
+//       if (authorization[0] !== 'Bearer') {
+//           return res.status(401).send();
+//       } else {
+//           req.jwt = jwt.verify(authorization[1], secret);
+//           return next();
+//       }
+//   } catch (err) {
+//       return res.status(403).send();
+//   }
+// } else {
+//   return res.status(401).send();
+// }
+
 };
 const authJwt = {
   verifyToken: verifyToken,
